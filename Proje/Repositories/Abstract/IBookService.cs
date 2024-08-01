7f9b4ae4e15709edb3fdf7877037c20d77256130
Eren Altan <154691461+ErnAltn@users.noreply.github.com>
2024-08-01 16:27:54 +0300

    Web küçük proje

---
+++ Proje/Repositories/Abstract/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using Proje.Models.Domain;
+
+namespace Proje.Repositories.Abstract
+{
+    public interface IBookService
+    {
+        bool Add(Book model);
+        bool Update(Book model);
+        bool Delete(int id);
+        Book FindById(int id);
+        IEnumerable<Book> GetAll();
+    }
+}
---
+++ Proje/Repositories/Abstract/IGenreService.cs
@@ -0,0 +1,14 @@
+﻿using Proje.Models.Domain;
+
+namespace Proje.Repositories.Abstract
+{
+    public interface IGenreService
+    {
+        bool Add(Genre model);
+        bool Update(Genre model);
+        bool Delete(int id);
+        Genre FindById(int id);
+        IEnumerable<Genre> GetAll();
+    }
+}
+
